2e7d816c644c54d5f6bfef9d2a1a6001ba3c44a8
akirepko1 <a.kirepko@agimagroup.ru>
2021-09-21 14:45:24 +0300

    Добавил проверк на наличие паролей для регистрации и авторизации

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,4 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 
 class MainPage(BasePage):
@@ -7,5 +6,3 @@ class MainPage(BasePage):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
 
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
